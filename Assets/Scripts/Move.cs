d0363a37a09714b2fb6fdd069d511977d6218882
Pierre Demessence <pierre.demessence@gmail.com>
2020-03-08 15:20:21 +0100

    Player now change animation while moving

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Move : MonoBehaviour
 {
     [InlineEditor] [SerializeField] private List<Movement> _movements;
+    [SerializeField] private MoveEnd _onMoveEnd;
+    [SerializeField] private MoveStart _onMoveStart;
+
     private Rigidbody2D _rigidbody2D;
+    private bool isMoving;
 
     private void Awake()
     {
@@ -17,5 +23,25 @@ public class Move : MonoBehaviour
     private void Update()
     {
         _rigidbody2D.velocity = _movements.FindAll(movement => movement).Aggregate(new Vector2(), (result, movement) => result + movement.Velocity());
+        if (!isMoving && _rigidbody2D.velocity != Vector2.zero)
+        {
+            isMoving = true;
+            _onMoveStart.Invoke();
+        }
+        else if (isMoving && _rigidbody2D.velocity == Vector2.zero)
+        {
+            isMoving = false;
+            _onMoveEnd.Invoke();
+        }
     }
+}
+
+[Serializable]
+internal class MoveStart : UnityEvent
+{
+}
+
+[Serializable]
+internal class MoveEnd : UnityEvent
+{
 }
