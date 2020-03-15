4df6ef3f473463a6652edc37cd65beda76b7e5f4
Pierre Demessence <pierre.demessence@gmail.com>
2020-03-15 20:18:38 +0100

    Usage of nameof()

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using JetBrains.Annotations;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -8,20 +7,19 @@ using UnityEngine;
 [RequireComponent(typeof(LookAt))]
 public class Enemy : MonoBehaviour
 {
-    [ValueDropdown("GetListOfFindTargets", AppendNextDrawer = true)] [SerializeField] [Required]
+    [ValueDropdown(nameof(GetListOfFindTargets), AppendNextDrawer = true)] [SerializeField] [Required]
     private FindTarget _findTarget;
     [SerializeField] [Required] private LookAt _lookAt;
     [SerializeField] [Required] private Move _move;
 
     public GameObject Target { get; private set; }
 
-    [UsedImplicitly]
     private IEnumerable<FindTarget> GetListOfFindTargets() => GetComponents<FindTarget>();
 
     private void Update()
     {
         Target = null;
-        if (_findTarget.Targets.Any()) Target = _findTarget.Targets?.First();
+        if (_findTarget.Targets != null && _findTarget.Targets.Any()) Target = _findTarget.Targets?.First();
 
         if (Target != null) _lookAt.SetTarget(Target.transform.position);
         else _lookAt.Stop();
